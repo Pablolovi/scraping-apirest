2b9567dd8e3068abf7b34a4187dba6e32d856a57
Pablolovi <lvizpablez@gmail.com>
2025-01-19 18:39:09 +0100

    Actualización código scraping

--- scraping.js
+++ scraping.js
@@ -1,39 +1,48 @@
-// Scraping de EL PAÍS
-
-const axios =  require('axios');
-const cheerio = require('cheerio');
+const axios =require('axios');
+const cheerio=require('cheerio');
+const express=require('express');
+const router=express.Router();
 const fs = require('fs');
 
-const url = 'https://elpais.com/ultimas-noticias/';
 
-async function scrapeNoticias() {
-    try {
-        const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
-        let noticias = [];
+const url=`https://elpais.com/ultimas-noticias/`
+       
 
-        $('.articulo').each((index, element) => {
-            const titulo = $(element).find('.titulo').text().trim();
-            const descripcion = $(element).find('.entradilla').text().trim();
-            const enlace = $(element).find('a').attr('src');
-            const imagen = $(element).find('img').attr('src');
 
-            const noticia = {
-                titulo,
-                imagen,
-                descripcion,
-                enlace: `https://elpais.com${enlace}`,
+router.get('/',(req,res)=>{
+    axios.get(url).then((response)=>{
+        if (response.status===200){
+         const htmlData= response.data;
+      
+           //Cheerio
+           const $=cheerio.load(htmlData);
+  
+           let noticias = [];
+           $('article.c.c-d.c--m').each((index,element) => {
+             const titulo = $(element).find('h2.c_t').text().trim();
+             const enlace = $(element).find('a').attr('href') ? `https://elpais.com${$(element).find('a').attr('href')}` : null;
+             const imagen = $(element).find('img').attr('src');
+             const descripcion = $(element).find('p').text().trim();
+       
+             if (titulo && enlace) {
+               noticias.push({
+                 titulo,
+                 imagen: imagen || 'Sin imagen',
+                 descripcion: descripcion || 'Sin descripción',
+                 enlace,
+               });
+             }
+           });
+       
+     fs.writeFileSync('noticias.json', JSON.stringify(noticias, null, 2));
+        
+        
+        }
+    })
+  })
+  
+ 
 
-            };
+ 
 
-            noticias.push(noticia);
-        });
-
-        fs.writeFileSync('noticias.json', JSON.stringify(noticias, null, 2));
-        console.log('Scraping completado y datos guardados en noticias.json');
-    } catch (error) {
-        console.error('Error al realizar el scraping:', error.message);
-    }
-}
-
-module.exports = scrapeNoticias;
+  module.exports=router ;
